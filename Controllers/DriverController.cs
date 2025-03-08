b969298a1c3bbcb9ca1a3333854b78f47707aebd
Dcarriong <alejocarriong@gmail.com>
2025-03-07 20:17:27 -0500

    1

---
+++ Controllers/DriverController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using F1Drivers.Models;
+using System;
+
+namespace F1DriversApp.Controllers
+{
+    public class DriverController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        private const string API_URL = "https://f1api.dev/api/drivers";
+
+        public DriverController()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<IActionResult> Index(int page = 1, int pageSize = 10)
+        {
+            var response = await _httpClient.GetStringAsync(API_URL);
+            var driverData = JsonConvert.DeserializeObject<ApiResponse>(response);
+
+            if (driverData.Drivers == null || !driverData.Drivers.Any())
+            {
+                Console.WriteLine("No se encontraron conductores.");
+            }
+
+            var drivers = driverData.Drivers;
+            var paginatedDrivers = drivers.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+
+            ViewBag.CurrentPage = page;
+            ViewBag.TotalPages = (int)Math.Ceiling((double)drivers.Count / pageSize);
+
+            return View("Index", paginatedDrivers);
+        }
+
+        public async Task<IActionResult> Details(string id)
+        {
+            var response = await _httpClient.GetStringAsync($"{API_URL}/{id}");
+
+            var driver = JsonConvert.DeserializeObject<Driver>(response);  
+
+            if (driver == null)
+            {
+                return NotFound();
+            }
+
+            return View(driver);  
+        }
+    }
+
+    public class ApiResponse
+    {
+        public List<Driver> Drivers { get; set; }
+    }
+}
---
+++ Models/Driver.cs
@@ -0,0 +1,12 @@
+﻿public class Driver
+{
+    public string DriverId { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Nationality { get; set; }
+
+    public string Birthday { get; set; }  
+    public string Number { get; set; }
+    public string ShortName { get; set; }
+    public string Url { get; set; }
+}
